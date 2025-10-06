eee8884e7bd97d4cd9df271ca77904780ad95356
Augusto <augustojvl8@gmail.com>
2025-10-06 16:33:27 -0300

    clase 8 de java

---
+++ Tecnicatura4Semestre/Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -0,0 +1,61 @@
+package utn.tienda_libros.vista;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import utn.tienda_libros.servicio.LibroServicio;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+@Component
+public class LibroFrom extends JFrame {
+    LibroServicio libroServicio;
+    private JPanel panel;
+    private JLabel Value;
+    private JTable tablaLibros;
+    private DefaultTableModel tablaModeloLibros;
+
+    @Autowired
+    public LibroFrom(LibroServicio libroServicio){
+        this.libroServicio = libroServicio;
+        iniciarForma();
+    }
+
+    private void iniciarForma(){
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(900, 700);
+
+        //para obtener las dimensiones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getWidth()/2);
+        setLocation(x, y);
+    }
+    private void createUIComponents() {
+        this.tablaModeloLibros = new DefaultTableModel(0, 5);
+        String[] cabecera = {"Id", "Libro", "Autor", "Precio", "Existencias"};
+        this.tablaModeloLibros.setColumnIdentifiers(cabecera);
+        //Instanciar el objeto de JTable
+        this.tablaLibros = new JTable(tablaModeloLibros);
+        listarLibros();
+    }
+    private void listarLibros(){
+        tablaModeloLibros.setRowCount(0);
+        var libros = libroServicio.listarLibros();
+        libros.forEach((libro) -> {
+            Object [] renglonLibro = {
+                    libro.getIdLibro(),
+                    libro.getNombreLibro(),
+                    libro.getAutor(),
+                    libro.getPrecio(),
+                    libro.getExistencias()
+            };
+            this.tablaModeloLibros.addRow(renglonLibro);
+        });
+    }
+
+}
